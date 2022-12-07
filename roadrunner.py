dde82654235eb9b42e49fb231feb7d3f28f85534
Devon VanDyk <devonvandyk@gmail.com>
2022-12-07 13:23:42 -0400

    update comments

--- roadrunner.py
+++ roadrunner.py
@@ -1,13 +1,16 @@
 # ROADRUNNER
 
-# A coyote is chasing a roadrunner and they start out 50 feet apart. If you know how fast they are both traveling, and how far the roadrunner is from safety, determine whether or not the coyote is able to catch the roadrunner. 
+# A coyote is chasing a roadrunner and they start out 50 feet apart. If you know how fast they are both traveling, 
+# and how far the roadrunner is from safety, determine whether or not the coyote is able to catch the roadrunner. 
+
 # Both animals and the roadrunner's safe place are on a straight line.
 
 # Task: 
 # Determine whether or not the roadrunner made it to safety.
 
 # Input Format: 
-# Three integer values, the first value represents the distance the roadrunner is from safety, then the roadrunner's speed (feet/second) and then the coyote's speed (feet/second).
+# Three integer values, the first value represents the distance the roadrunner is from safety, 
+# then the roadrunner's speed (feet/second) and then the coyote's speed (feet/second).
 
 # Output Format: 
 # A string that says 'Meep Meep' if the roadrunner made it, or 'Yum' if the coyote caught up to the roadrunner.
